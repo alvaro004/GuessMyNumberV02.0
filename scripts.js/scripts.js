bb02087494fc34879bb1912d7e57ca440b069a0d
Alvaro Llano <alvarollano04@gmail.com>
2022-08-23 19:06:58 -0400

    Fixing currets bugs

--- scripts.js/scripts.js
+++ scripts.js/scripts.js
@@ -137,8 +137,6 @@ const active = document.querySelector('.box');
 const closeModal = document.querySelectorAll('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-console.log(closeModal);
-
 const closeModalFunc = () => {
   active.classList.remove('active');
   overlay.classList.remove('overlay-active');
